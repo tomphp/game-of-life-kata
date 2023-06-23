547aad775304488915e00849f6ff1261e774f0d6
Tom Oram <tom.oram@armakuni.com>
2023-06-23 14:49:29 +0100

    end of episode 2

--- src/grid.py
+++ src/grid.py
@@ -2,6 +2,15 @@ from typing import List
 
 import numpy as np
 
+DEAD = 0
+LIVE = 1
+
+NORTH = (0, 1)
+NORTH_EAST = (0,2)
+EAST = (1, 2)
+SOUTH = (2, 1)
+WEST = (1, 0)
+
 
 class Grid:
     matrix: List[List[int]]
@@ -14,11 +23,17 @@ class Grid:
         self.matrix = matrix
 
     def update(self):
-        newgrid = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-        ]
+        number_of_live_neighbours = self._count_live_neighbours()
 
-        return Grid(newgrid)
+        if number_of_live_neighbours == 2:
+            return Grid([[DEAD, DEAD],
+                         [DEAD, LIVE]])
 
+        return Grid([[DEAD, DEAD],
+                     [DEAD, DEAD]])
+
+    def _count_live_neighbours(self):
+        neighbours = [NORTH, NORTH_EAST, SOUTH, EAST, WEST]
+        neighbour_states = [self.matrix[row][column] for row, column in neighbours]
+        number_of_live_neighbours = len([state for state in neighbour_states if state == LIVE])
+        return number_of_live_neighbours
--- test/test_game.py
+++ test/test_game.py
@@ -1,15 +1,131 @@
 from unittest import TestCase
-from testfixtures import compare
 
-from src.grid import Grid
+from src.grid import *
 
 
 class TestGrid(TestCase):
-    def test_update_returns_empty_grid_when_grid_empty(self):
+    def test_update_returns_dead_cell_when_all_neighbours_are_dead(self):
         g = Grid([
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
+            [DEAD, DEAD, DEAD],
+            [DEAD, DEAD, DEAD],
+            [DEAD, DEAD, DEAD],
         ])
 
-        compare(g.update(), g)
+        resulting_grid = g.update()
+
+        assert resulting_grid.matrix[1][1] == DEAD
+
+    def test_update_returns_live_cell_when_two_neighbours_are_live(self):
+        g = Grid([
+            [DEAD, LIVE, DEAD],
+            [DEAD, LIVE, DEAD],
+            [DEAD, LIVE, DEAD],
+        ])
+
+        resulting_grid = g.update()
+
+        assert resulting_grid.matrix[1][1] == LIVE
+
+    def test_update_returns_dead_cell_north_neighbour_is_alive(self):
+        g = Grid([
+            [DEAD, LIVE, DEAD],
+            [DEAD, LIVE, DEAD],
+            [DEAD, DEAD, DEAD],
+        ])
+
+        resulting_grid = g.update()
+
+        assert resulting_grid.matrix[1][1] == DEAD
+
+    def test_update_returns_dead_cell_one_south_neighbour_is_alive(self):
+        g = Grid([
+            [DEAD, DEAD, DEAD],
+            [DEAD, LIVE, DEAD],
+            [DEAD, LIVE, DEAD],
+        ])
+
+        resulting_grid = g.update()
+
+        assert resulting_grid.matrix[1][1] == DEAD
+
+    def test_update_returns_live_cell_when_two_horizontal_neighbours_are_alive(self):
+        g = Grid([
+            [DEAD, DEAD, DEAD],
+            [LIVE, LIVE, LIVE],
+            [DEAD, DEAD, DEAD],
+        ])
+
+        resulting_grid = g.update()
+
+        assert resulting_grid.matrix[1][1] == LIVE
+
+    def test_update_returns_dead_cell_when_west_neighbours_are_alive(self):
+        g = Grid([
+            [DEAD, DEAD, DEAD],
+            [LIVE, LIVE, DEAD],
+            [DEAD, DEAD, DEAD],
+        ])
+
+        resulting_grid = g.update()
+
+        assert resulting_grid.matrix[1][1] == DEAD
+
+    def test_update_returns_dead_cell_when_east_neighbours_are_alive(self):
+        g = Grid([
+            [DEAD, DEAD, DEAD],
+            [DEAD, LIVE, LIVE],
+            [DEAD, DEAD, DEAD],
+        ])
+
+        resulting_grid = g.update()
+
+        assert resulting_grid.matrix[1][1] == DEAD
+
+    def test_update_returns_live_cell_when_north_and_east_neighbours_are_alive(self):
+        g = Grid([
+            [DEAD, LIVE, DEAD],
+            [DEAD, LIVE, LIVE],
+            [DEAD, DEAD, DEAD],
+        ])
+
+        resulting_grid = g.update()
+
+        assert resulting_grid.matrix[1][1] == LIVE
+
+    def test_update_returns_live_cell_when_north_and_northeast_neighbours_are_alive(self):
+        g = Grid([
+            [DEAD, LIVE, LIVE],
+            [DEAD, LIVE, DEAD],
+            [DEAD, DEAD, DEAD],
+        ])
+
+        resulting_grid = g.update()
+
+        assert resulting_grid.matrix[1][1] == LIVE
+
+    def test_live_cell_with_two_live_neighbours_stays_alive(self):
+        test_cases = [
+            [NORTH, SOUTH],
+            [EAST, WEST],
+            [NORTH, EAST],
+            [EAST, SOUTH],
+            [SOUTH, WEST],
+            [WEST, NORTH],
+            [NORTH, NORTH_EAST]
+        ]
+        for live_neighbours in test_cases:
+            g = create_grid_with_live_neighbours(live_neighbours)
+            resulting_grid = g.update()
+            assert resulting_grid.matrix[1][1] == LIVE
+
+
+def create_grid_with_live_neighbours(live_neighbours):
+    a = [
+        [DEAD, DEAD, DEAD],
+        [DEAD, LIVE, DEAD],
+        [DEAD, DEAD, DEAD],
+    ]
+    for row, col in live_neighbours:
+        a[row][col] = LIVE
+    g = Grid(a)
+    return g
